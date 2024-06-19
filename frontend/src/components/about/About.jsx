977d9a2475db57ba17491747b868b7992fd97223
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-19 15:57:24 -0400

    version2 mvp

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -15,32 +15,41 @@ export const About = () => {
               <span className=" bg-indigo-500  text-white py-2 px-3 rounded font-bold mr-1 float-left text-2xl">
                 H
               </span>
-              ey! I&apos;m Ryan, if you haven&apos;t already gathered that by
-              now. I specialize in frontend development, primarily React.js, but
-              I also have experience with backend technologies like Node.js and
-              Express.js. I live in Greenville, SC.
+              ey! I'm Ryan McGatha, a passionate software developer with a knack
+              for creating efficient and innovative web applications. My journey
+              in web development began at Carolina Code School, where I
+              completed a comprehensive Full Stack Web Development course,
+              equipping me with a solid foundation in technologies like
+              React.js, Python, and PostgreSQL.
             </p>
           </Reveal>
           <Reveal>
             <p className="leading-relaxed text-zinc-300">
-              I&apos;m currently working on several projects, including a chat
-              application called Push It. I enjoy taking on new challenges and
-              learning new technologies to build efficient and scalable
-              applications.
+              I specialize in building dynamic and responsive web applications,
+              leveraging my skills in React, Express.js, Node.js, and
+              PostgreSQL. One of my notable projects is the Push It chat
+              application, which showcases my ability to develop complex
+              messaging systems with real-time updates and secure,
+              user-protected chats.
             </p>
           </Reveal>
           <Reveal>
             <p className="leading-relaxed text-zinc-300">
-              Outside of coding, I have a passion for fishing and often spend my
-              weekends on the water. I also enjoy tinkering with my vehicle’s
-              sound system and exploring the latest tech gadgets.
+              In addition to my technical expertise, I've worked on various
+              projects that demonstrate my ability to learn and adapt quickly
+              under pressure. For instance, I was contracted to build an
+              Internal Franchise Document Management System app for Sully's
+              Steamers, where I played a pivotal role in its development and
+              deployment.
             </p>
           </Reveal>
           <Reveal>
             <p className="leading-relaxed text-zinc-300">
-              I&apos;m actively looking for new opportunities where I can
-              leverage my skills in web development. If you have an opening that
-              you think I&apos;d be a good fit for, let&apos;s connect 🔗
+              Outside of coding, I enjoy fishing and exploring new technologies.
+              I believe in continuous learning and am always on the lookout for
+              opportunities to grow and refine my skills. Whether it's
+              collaborating on a new project or diving into a challenging
+              problem, I thrive on creating solutions that make a difference.
             </p>
           </Reveal>
           <Reveal>
